ad5fd161ee0cffa732b18d8e636cd5f2ed91f192
Anna Abdeeva <abanna1999@gmail.com>
2022-02-24 00:08:54 +0700

    Lab-1 is done

---
+++ iOS_Lab-1/iOS_Lab-1/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  iOS_Lab-1
+//
+//  Created by Anna Abdeeva on 22.02.2022.
+//
+
+import Foundation
+
+var forest = Forest()
+print (forest.arrayOfForestDwellers)
+print ("_____________")
+forest.daySmulation()
